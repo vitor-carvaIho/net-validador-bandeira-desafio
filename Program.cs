6966fff3164b7f102f7d934e25c20412a4dd38f6
vitor-carvaIho <77708828+vitor-carvaIho@users.noreply.github.com>
2026-01-11 23:41:41 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//Regex feito manualmente: EnRoute, Aura
+//TO DO:pensar num jeito de ignorar os espaços
+using Validador.Models;
+
+ValidadorBandeira entrada = new ValidadorBandeira(bandeira:"5413694826729220");
+entrada.Validador();
